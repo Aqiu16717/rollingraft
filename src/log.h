4208d2b6817c76ad58d152588a99d102c44d1714
aq1u <aq1u@outlook.com>
2024-04-29 19:24:51 +0800

    add log.h and server.h

---
+++ src/log.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace rollingraft {
+
+struct LogEntry;
+
+class Log {
+public:
+private:
+    std::vector<LogEntry> entries_;
+};
+
+struct LogEntry {
+    LogEntry() = default;
+    LogEntry(uint32_t index, uint32_t term, const std::string& command)
+        : index_(index), term_(term), command_(command) {}
+
+    uint32_t index_;
+    uint32_t term_;
+    std::string data_;
+};
+
+} // namespace RollingRaft
---
+++ src/server.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace rollingraft {
+
+class Server {
+public:
+private:
+    uint32_t current_term_;
+};
+
+} // namespace RollingRaft

904f95fd717506517432efb607984a74f69690ab
aq1u <aq1u@outlook.com>
2025-05-13 12:44:17 +0800

    implement status

--- include/rollingraft/status.h
+++ include/rollingraft/status.h
@@ -2,13 +2,33 @@
 
 namespace rollingraft {
 
+// most code steal from leveldb
 class Status {
 public:
+    // Create a success status.
+    Status() noexcept : state_(nullptr) {}
+    ~Status() {
+        delete[] state_;
+    }
+
+    Status(const Status& rhs);
+    Status& operator=(const Status& rhs);
+
+    Status(Status&& rhs) noexcept : state_(rhs.state_) {
+        rhs.state_ = nullptr;
+    }
+    Status& operator=(Status&& rhs) noexcept;
+
+    // Return a success status.
+    static Status OK() { return Status(); }
+
+    // Returns true iff the status indicates success.
+    bool ok() const { return (state_ == nullptr); }
+
 private:
     enum Code {
         KOk = 0
     };
-    // steal from leveldb
     // OK status has a null state_
     // Otherwise, state_ is a new[] array of the following from
     // state_[0..3]  == length of message

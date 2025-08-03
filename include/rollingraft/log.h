e574743a3db4600c62033c624d6475e4413ccefc
aq1u <aq1u@outlook.com>
2025-08-03 20:50:14 +0800

    refactor: move LogEntry struct definition before Log class

--- include/rollingraft/log.h
+++ include/rollingraft/log.h
@@ -7,7 +7,16 @@
 
 namespace rollingraft {
 
-struct LogEntry;
+struct LogEntry {
+    LogEntry() = default;
+    LogEntry(uint32_t index, uint32_t term, const std::string& command)
+        : index_(index), term_(term), command_(command) {}
+
+    uint32_t index_;
+    uint32_t term_;
+    std::string data_;
+    std::string command_;
+};
 
 class Log {
 public:
@@ -22,15 +31,5 @@ private:
     std::vector<LogEntry> entries_;
 };
 
-struct LogEntry {
-    LogEntry() = default;
-    LogEntry(uint32_t index, uint32_t term, const std::string& command)
-        : index_(index), term_(term), command_(command) {}
-
-    uint32_t index_;
-    uint32_t term_;
-    std::string data_;
-    std::string command_;
-};
 
 } // namespace rollingraft

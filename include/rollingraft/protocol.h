f598abb3911841f59af26843c47e5f2651731799
aq1u <aq1u@outlook.com>
2025-08-31 15:31:48 +0800

    style: code formatting

--- include/rollingraft/protocol.h
+++ include/rollingraft/protocol.h
@@ -1,20 +1,25 @@
 #pragma once
 
 #include <string>
+
 #include "rollingraft/rpc.h"
 #include "rollingraft/status.h"
 
 namespace rollingraft {
 
 class Protocol {
-public:
-    virtual ~Protocol() = default;
-    
-    virtual Status SerializeRequest(const RaftRequest& req, std::string& output) const = 0;
-    virtual Status DeserializeRequest(const std::string& input, RaftRequest& req) = 0;
-    
-    virtual Status SerializeResponse(const RaftResponse& res, std::string& output) const = 0;
-    virtual Status DeserializeResponse(const std::string& input, RaftResponse& res) = 0; 
+ public:
+  virtual ~Protocol() = default;
+
+  virtual Status SerializeRequest(const RaftRequest& req,
+                                  std::string& output) const = 0;
+  virtual Status DeserializeRequest(const std::string& input,
+                                    RaftRequest& req) = 0;
+
+  virtual Status SerializeResponse(const RaftResponse& res,
+                                   std::string& output) const = 0;
+  virtual Status DeserializeResponse(const std::string& input,
+                                     RaftResponse& res) = 0;
 };
 
-} // namespace rollingraft
+}  // namespace rollingraft
--- src/json_protocol.h
+++ src/json_protocol.h
@@ -3,10 +3,14 @@
 using namespace rollingraft;
 
 class JsonProtocol : public Protocol {
-public:
-    Status SerializeRequest(const RaftRequest& req, std::string& output) const override;
-    Status DeserializeRequest(const std::string& input, RaftRequest& req) override;
-    
-    Status SerializeResponse(const RaftResponse& res, std::string& output) const override;
-    Status DeserializeResponse(const std::string& input, RaftResponse& res) override;
+ public:
+  Status SerializeRequest(const RaftRequest& req,
+                          std::string& output) const override;
+  Status DeserializeRequest(const std::string& input,
+                            RaftRequest& req) override;
+
+  Status SerializeResponse(const RaftResponse& res,
+                           std::string& output) const override;
+  Status DeserializeResponse(const std::string& input,
+                             RaftResponse& res) override;
 };

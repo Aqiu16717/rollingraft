dc08be19e0fc4260d7194197d7ee60b338c6609b
aq1u <aq1u@outlook.com>
2025-09-26 12:28:39 +0800

    feat: improve logger framework usability and structure

--- include/rollingraft/logger.h
+++ include/rollingraft/logger.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <format>
 #include <string>
 
 namespace rollingraft {
@@ -21,5 +22,49 @@ class Logger {
   void Error(const std::string& message) { Log(LogLevel::ERROR, message); }
   void Fatal(const std::string& message) { Log(LogLevel::FATAL, message); }
 };
+
+class LoggerFactory {
+ public:
+  LoggerFactory(const LoggerFactory&) = delete;
+  LoggerFactory& operator=(const LoggerFactory&) = delete;
+  LoggerFactory(LoggerFactory&&) = delete;
+  LoggerFactory& operator=(LoggerFactory&&) = delete;
+
+  static LoggerFactory& Instance() {
+    static LoggerFactory instance;
+    return instance;
+  }
+
+  Logger* GetLogger();
+  void SetLogger(std::unique_ptr<Logger> logger);
+
+ private:
+  LoggerFactory() = default;
+  ~LoggerFactory() = default;
+
+  mutable std::mutex mutex_;
+  std::unique_ptr<Logger> logger_;
+};
+
+#define LOG_MESSAGE(level, fmt_str, ...)                       \
+  do {                                                         \
+    Logger* logger = LoggerFactory::Instance().GetLogger();    \
+    if (logger && logger->GetLogLevel() <= level) {            \
+      logger->Log(level, std::format(fmt_str, ##__VA_ARGS__)); \
+    }                                                          \
+  } while (0)
+
+#define LOG_TRACE(fmt_str, ...) \
+  LOG_MESSAGE(rollingraft::LogLevel::TRACE, fmt_str, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt_str, ...) \
+  LOG_MESSAGE(rollingraft::LogLevel::DEBUG, fmt_str, ##__VA_ARGS__)
+#define LOG_INFO(fmt_str, ...) \
+  LOG_MESSAGE(rollingraft::LogLevel::INFO, fmt_str, ##__VA_ARGS__)
+#define LOG_WARN(fmt_str, ...) \
+  LOG_MESSAGE(rollingraft::LogLevel::WARN, fmt_str, ##__VA_ARGS__)
+#define LOG_ERROR(fmt_str, ...) \
+  LOG_MESSAGE(rollingraft::LogLevel::ERROR, fmt_str, ##__VA_ARGS__)
+#define LOG_FATAL(fmt_str, ...) \
+  LOG_MESSAGE(rollingraft::LogLevel::FATAL, fmt_str, ##__VA_ARGS__)
 
 }  // namespace rollingraft
--- src/logger.cpp
+++ src/logger.cpp
@@ -1,10 +1,11 @@
-#include "logger_factory.h"
+#include "rollingraft/logger.h"
 #include "logger_spdlog_adapter.h"
 #include <memory>
 
 using namespace rollingraft;
 
 Logger* LoggerFactory::GetLogger() {
+  std::lock_guard<std::mutex> lock(mutex_);
   if (!logger_) {
     logger_ = std::make_unique<SpdlogAdapter>();
   }
@@ -12,5 +13,6 @@ Logger* LoggerFactory::GetLogger() {
 }
 
 void LoggerFactory::SetLogger(std::unique_ptr<Logger> logger) {
+  std::lock_guard<std::mutex> lock(mutex_);
   logger_ = std::move(logger);
 }

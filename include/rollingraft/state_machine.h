cc798d0234c6c1eb33717ec8905bbb21abad29a5
aq1u <aq1u@outlook.com>
2026-03-22 14:05:38 +0800

    feat: update state machine

--- include/rollingraft/state_machine.h
+++ include/rollingraft/state_machine.h
@@ -2,14 +2,15 @@
 namespace rollingraft {
 
 struct SnapshotMeta {
-  uint64_t last_included_index;
-  uint64_t last_included_term;
+  uint64_t last_included_index_;
+  uint64_t last_included_term_;
 };
 
 // read-only readview
 class Snapshot {
  public:
   virtual ~Snapshot() = default;
+
   virtual const SnapshotMeta& GetMeta() const = 0;
 
   virtual size_t Read(uint64_t offset, uint8_t* dest, size_t length) = 0;
@@ -19,6 +20,7 @@ class Snapshot {
 
 struct ApplyResult {
   bool success_;
+  // data that return to client
   std::string response_;
   uint64_t applied_index_;
 };
@@ -28,12 +30,21 @@ class StateMachine {
   StateMachine() = default;
   virtual ~StateMachine() = default;
 
-  virtual ApplyResult Apply(const std::vector<uint8_t>& data,
-                            uint64_t index) = 0;
+  // write
+  virtual ApplyResult Apply(std::span<const uint8_t> data, uint64_t index) = 0;
+
+  // read
   virtual uint64_t GetLastAppliedIndex() const = 0;
 
-  virtual std::vector<uint8_t> Snapshot() = 0;
+  // create snapshot handle, lightweight, fast, and nonblock
+  virtual std::shared_ptr<Snapshot> CreateSnapshot() = 0;
+
+  // load from outside
   virtual bool Restore(const std::vector<uint8_t>& snapshot) = 0;
+
+  // for linearizable reads: block or callback until the index reaches the
+  // target value
+  virtual void WaitIndex(uint64_t index, std::function<void()> cb) = 0;
 };
 
 }  // namespace rollingraft

bea5d9ae6074c3381bc907bea780a9cff804973f
aq1u <aq1u@outlook.com>
2026-03-22 14:22:57 +0800

    feat: update counter example, implement CounterSnapshot

--- example/counter/counter.cpp
+++ example/counter/counter.cpp
@@ -2,6 +2,38 @@
 #include <memory>
 #include <rollingraft/raft_node.h>
 
+class CounterSnapshot : pulic Snapshot {
+ public:
+  CounterSnapshot(int64_t value, uint64_t index, uint64_t term)
+      : value_(value) {
+    meta_.last_included_index_ = index;
+    meta_.last_included_term_ = term;
+
+    data_.resize(sizeof(int64_t));
+    std::memcpy(data_.data(), &value_, sizeof(int64_t));
+  }
+
+  ~CounterSnapshot() = default;
+
+  const SnapshotMeta& GetMeta() const override { return meta_; }
+
+  size_t Read(uint64_t offset, uint8_t* dest, size_t length) override {
+    if (offset > data_.size()) {
+      return 0;
+    }
+    size_t can_read = std::min(data.size() - offset, length);
+    std::memcpy(dest, data_.data() + offset, can_read);
+    return can_read;
+  }
+
+  std::string GetPath() const override { return ""; }
+
+ private:
+  SnapshotMeta meta_;
+  int64_t value_;
+  std::vector<uint8_t> data_;
+};
+
 class CounterMachine : Public StateMachine {
  public:
 };
